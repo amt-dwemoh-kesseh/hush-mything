7876f432b9f534c3f0d38c272582fd6452d19eca
Dwemoh-Kesseh Emmanuel <emmanuel.dwemoh-kesseh@amalitech.org>
2023-05-09 09:30:40 +0100

    truetofalse

--- src/routes/User/customer.route.ts
+++ src/routes/User/customer.route.ts
@@ -1,14 +1,10 @@
-import express,{Request,Response, response} from "express";
+import express, { Request, Response, response } from "express";
 import { customerSignIn } from "../../controllers/User/register/customer.register";
-import {
-  customerRegistrationValidation,
-  
-} from "../../middlewares/validators/auth.validator";
+import { customerRegistrationValidation } from "../../middlewares/validators/auth.validator";
 import { verifymyAccount } from "../../controllers/User/authenticate/accountVerify";
 import { validateEmail } from "../../config/validatorConfig";
 import { sendResetPasswordMail } from "../../controllers/User/resetPassword/customer.passwordReset";
 import { verifyMyPasswordReset } from "../../controllers/User/authenticate/password.reset";
-
 
 export const customerRouter = express.Router();
 
@@ -17,11 +13,8 @@ customerRouter.post(
   customerRegistrationValidation,
   customerSignIn
 );
-customerRouter.get("/verify",verifymyAccount)
+customerRouter.put("/verify", verifymyAccount);
 
-
-
-customerRouter.post('/reset', validateEmail, sendResetPasswordMail);
+customerRouter.post("/reset", validateEmail, sendResetPasswordMail);
 
 customerRouter.put("/verify/reset", verifyMyPasswordReset);
-
